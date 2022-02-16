a92de66a7cb1ac6c08c12f2c1bd51644174eda2c
tauhid495 <96792668+tauhid495@users.noreply.github.com>
2022-02-16 08:09:16 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+/* // color change by id
+document.getElementById('topPlayers').style.color = "rgb(236, 45, 1)";
+document.getElementById('topBlogs').style.color = "rgb(236, 45, 1)";
+
+// background color set by class
+const players = document.getElementsByClassName('player');
+for (const player of players) {
+    player.style.backgroundColor = 'tomato';
+} */
+
